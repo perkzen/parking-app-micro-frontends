dbca968727b16a9ed23a585e00dbb7fd3ebe205d
perkzen <perko.domen@gmail.com>
2024-04-16 11:47:21 +0200

    feat: make tw work

--- parking/src/CreateParkingSpot.tsx
+++ parking/src/CreateParkingSpot.tsx
@@ -30,7 +30,7 @@ const CreateParkingSpot = () => {
 
     return (
         <div className="max-w-md mx-auto">
-            <h1>Create parking spot form</h1>
+            <h1 className={"text-red-500"}>Create parking spot form</h1>
             <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="location">
